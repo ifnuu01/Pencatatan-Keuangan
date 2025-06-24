fe79e6f93aa5b1eef3aab715ef6b6ff4a66f6631
ifnuu01 <ifnuu01@gmail.com>
2025-06-24 19:23:09 +0800

    feat : add constant menu items

---
+++ src/constant/menuItems.jsx
@@ -0,0 +1,9 @@
+const menuItems = [
+  { label: "Dashboard", icon: "mdi:home", path: "/dashboard" },
+  { label: "Transaksi", icon: "mdi:cash-multiple", path: "/transaksi" },
+  { label: "Kategori", icon: "mdi:format-list-bulleted", path: "/kategori" },
+  { label: "Anggaran", icon: "mdi:calculator", path: "/anggaran" },
+  { label: "Dompet", icon: "mdi:wallet", path: "/dompet" },
+];
+
+export default menuItems;
